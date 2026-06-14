c7ca396529983caae073c694bf6da868041712d7
Yunus <shaibuyunus115@gmail.com>
2026-06-14 01:33:55 +0100

    json-server error resolved

--- task-4/backend/server.js
+++ task-4/backend/server.js
@@ -4,14 +4,14 @@ import { fileURLToPath } from 'url';
 
 const server = jsonServer.create();
 
-// Create your own __dirname equivalent for ES Modules
+// Deriving __dirname for ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
-// Use 5000 or fallback to Render's dynamic environment port
+// Render sets the PORT dynamically via environment variables
 const port = process.env.PORT || 5000;
 
 server.use(middlewares);
