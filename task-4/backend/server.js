d343e68226d0d19953e9a11e3e6dc658d204de75
Yunus <shaibuyunus115@gmail.com>
2026-06-14 01:06:34 +0100

    Error logproblem

--- task-4/backend/server.js
+++ task-4/backend/server.js
@@ -1,11 +1,18 @@
-const jsonServer = require('json-server');
-const path = require('path');
+import jsonServer from 'json-server';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const server = jsonServer.create();
+
+// Create your own __dirname equivalent for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
-const port = process.env.PORT || 3001;
+// Use 5000 or fallback to Render's dynamic environment port
+const port = process.env.PORT || 5000;
 
 server.use(middlewares);
 server.use(router);
